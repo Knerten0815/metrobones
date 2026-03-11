e4bab95213da3a672a9dbcd7387e875ddbb88081
Kevin Zielke <kevin_zielke@yahoo.de>
2026-03-11 07:39:28 +0100

    DRYed up lifecycle methods of metronome playing pages.

---
+++ Metrobones/Layout/MetronomePageBase.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Components;
+using Metrobones.Services;
+
+namespace Metrobones.Layout;
+
+public abstract class MetronomePageBase : ComponentBase, IAsyncDisposable
+{
+    [Inject]
+    protected Metronome Met { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (Met.IsRunning)
+            await Met.Stop();
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            Met.BeatCallback += OnStateChanged;
+            Met.StopCallback += OnStateChanged;
+            await Met.Initialize();
+        }
+    }
+
+    private async void OnStateChanged()
+    {
+        await InvokeAsync(StateHasChanged);
+    }
+
+    public virtual async ValueTask DisposeAsync()
+    {
+        Met.StopCallback -= OnStateChanged;
+        Met.BeatCallback -= OnStateChanged;
+        await Met.Stop();
+    }
+}
